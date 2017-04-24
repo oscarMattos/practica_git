6092f4a300bce8aa2228075b52336de551836f24
Oscar Marcelo Mattos Zarandón <mattoszarandonoscar@gmail.com>
2017-04-24 19:33:51 -0300

    Se agrego la funcionalidad de la navegacion lateral junto con los estilos

--- app.js
+++ app.js
@@ -1,37 +1,60 @@
 
-var estadoBoxShadow = true;
-var estadoBoxSizing = true;
+// Variables globales
+var stateBoxShadow = true;
+var stateBoxSizing = true;
 
+// Funcion para hacer box-shadow
 function boxShadowToggle() {
   let main = document.querySelector("#main");
   let buttonMain = document.querySelector("#main_button");
-  if (estadoBoxShadow) {
+  if (stateBoxShadow) {
     main.style.transition = ".5s";
     main.classList.add("boxShadow");
     buttonMain.innerHTML = "remove box shadow";
-    estadoBoxShadow = false;
+    stateBoxShadow = false;
   } else {
     main.classList.remove("boxShadow");
     buttonMain.innerHTML = "add box shadow";
-    estadoBoxShadow = true;
+    stateBoxShadow = true;
   }
 }
 
 document.querySelector("#main_button").addEventListener("click", boxShadowToggle);
 
+// Funcion para hacer box-sizing
 function boxSizingToggle() {
   let header = document.querySelector("#header");
   let buttonHeader = document.querySelector("#header_button");
-  if (estadoBoxSizing) {
+  if (stateBoxSizing) {
     header.style.width = "50%";
     header.style.transition = "1s";
     buttonHeader.innerHTML = "expand box";
-    estadoBoxSizing = false;
+    stateBoxSizing = false;
   } else {
     header.style.width = "100%";
     buttonHeader.innerHTML = "reduce box";
-    estadoBoxSizing = true;
+    stateBoxSizing = true;
   }
 }
 
 document.querySelector("#header_button").addEventListener("click", boxSizingToggle);
+
+// Funciones para abrir y cerrar la navegacion lateral
+
+function openNav() {
+    let sideNav = document.querySelector("#sideNav");
+    let darkBackground = document.querySelector("#darkBackground");
+    sideNav.style.width = "250px";
+    darkBackground.style.display = "block";
+}
+
+document.querySelector("#openNav").addEventListener("click", openNav);
+
+function closeNav() {
+    let sideNav = document.querySelector("#sideNav");
+    let darkBackground = document.querySelector("#darkBackground");
+    sideNav.style.width = "0";
+    darkBackground.style.display = "none";
+}
+
+document.querySelector("#closeNav").addEventListener("click", closeNav);
